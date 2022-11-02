e41f7ddb205b696842d52e8640b1b90ffb42911f
Felix <barosiofelix@gmail.com>
2022-11-02 08:35:09 +0300

    Completed Asignment

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,26 @@
 class Dev < ActiveRecord::Base
 
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+
+    def received_one?(item_name)
+
+        self.freebies.each do |freebie|
+            return freebie.item_name == item_name
+        end
+
+    end
+
+    def give_away(dev, freebie)
+
+        if self.freebies.include?(freebie)
+            freebie.dev = dev
+            freebie.save
+        else
+            'Freebie is not available'
+        end
+
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,4 +18,9 @@ puts "Creating freebies..."
 # ***************************************************************
 # Create freebies Here
 
+Freebie.create(item_name: 'Smart Watch', value: 80, company_id: rand(1..4), dev_id: rand(1..4))
+Freebie.create(item_name: 'Laptop', value: 150, company_id: rand(1..4), dev_id: rand(1..4))
+Freebie.create(item_name: 'Bean bag', value: 30, company_id: rand(1..4), dev_id: rand(1..4))
+Freebie.create(item_name: 'T-Shirt', value: 10, company_id: rand(1..4), dev_id: rand(1..4))
+
 puts "Seeding done!"
